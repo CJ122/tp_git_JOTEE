b1467c28aeb6f6d2469376f7835427da92a4a23e
CJ122 <109337624+CJ122@users.noreply.github.com>
2022-07-20 16:40:14 +0400

    Update addTwoInt.py

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 
-def Add(num1, num2):
-  x = num1+num2
+def add(a, b):
+  x = a + b 
   return x
 
 import sys
 print (sys.argv)
 if len(sys.argv) == 3:
-  num1=float(sys.argv[1])
-  num2=float(sys.argv[2])
+  a =float(sys.argv[1])
+  b =float(sys.argv[2])
 if len(sys.argv) > 3:
   print ("Invalid number of arguments")
 elif len(sys.argv) == 2:
  print ("An argument is missing.")
- num1=float(sys.argv[1])
- num2 = float(input("Please enter argument 2: "))
-print ("The sum is:", Add(num1,num2))
+ a =float(sys.argv[1])
+ b = float(input("Please enter argument 2: "))
+print ("The sum is:", add(a, b))
 
